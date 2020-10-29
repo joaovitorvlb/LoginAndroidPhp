b489eda942018da97a899883379e059b75d88b67
Joao Vitor Lansoni Barbosa <joaovitorvlb@hotmail.com>
2020-10-29 13:44:38 -0300

    Testando start Activity

--- LogonAndroidPhp/app/src/main/java/com/example/logonandroidphp/MainLogin.java
+++ LogonAndroidPhp/app/src/main/java/com/example/logonandroidphp/MainLogin.java
@@ -2,13 +2,32 @@ package com.example.logonandroidphp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainLogin extends AppCompatActivity {
+
+    EditText editEmailLogger, editSenhaLogar;
+    Button btnEntrar;
+    TextView txtCadastro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_login);
+
+        txtCadastro = findViewById(R.id.txtCadastrar);
+
+        txtCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent abreCad = new Intent(MainLogin.this, MainCadastro.class);
+                startActivity(abreCad);
+            }
+        });
     }
 }
