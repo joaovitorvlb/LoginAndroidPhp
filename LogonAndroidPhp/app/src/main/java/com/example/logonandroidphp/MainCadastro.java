673529acbeae2ff9e332a83b4a87cad5e9135d9a
Joao Vitor Lansoni Barbosa <joaovitorvlb@hotmail.com>
2020-10-29 18:49:17 -0300

    Adicionado teste de conexao com o servitor

--- LogonAndroidPhp/app/src/main/java/com/example/logonandroidphp/MainCadastro.java
+++ LogonAndroidPhp/app/src/main/java/com/example/logonandroidphp/MainCadastro.java
@@ -6,6 +6,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.gson.JsonObject;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
 
 public class MainCadastro extends AppCompatActivity {
 
@@ -26,6 +31,22 @@ public class MainCadastro extends AppCompatActivity {
         btnCadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                String url = "http://env-9429261.jelastic.saveincloud.net/login/teste.json";
+
+                Ion.with(MainCadastro.this)
+                        .load(url)
+                        .asJsonObject()
+                        .setCallback(new FutureCallback<JsonObject>() {
+                            @Override
+                            public void onCompleted(Exception e, JsonObject result) {
+                                try {
+                                    Toast.makeText(MainCadastro.this, "Retorno" + result.toString(), Toast.LENGTH_LONG).show();
+                                } catch (Exception erro){
+                                    Toast.makeText(MainCadastro.this, "Ocorreu um erro" + erro, Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
                 
             }
         });
