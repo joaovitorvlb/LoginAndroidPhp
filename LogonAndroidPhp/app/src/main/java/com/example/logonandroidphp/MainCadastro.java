30b9dc4fc73f245802e7a611e7c0da2b1bf2b990
Joao Vitor Lansoni Barbosa <joaovitorvlb@hotmail.com>
2020-10-29 13:37:54 -0300

    Criado tela de login e cadastro

---
+++ LogonAndroidPhp/app/src/main/java/com/example/logonandroidphp/MainCadastro.java
@@ -0,0 +1,14 @@
+package com.example.logonandroidphp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainCadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_cadastro);
+    }
+}
